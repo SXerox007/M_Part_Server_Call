872c8a2b5f9b9aab92015dc0812bec5de318af72
SUMIT_THAKUR <SXerox007>
2017-05-10 17:06:58 +0530

    Model Class Add

--- app/src/main/java/com/skeleton/fragment/LoginFragment.java
+++ app/src/main/java/com/skeleton/fragment/LoginFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.skeleton.R;
 
@@ -12,11 +14,24 @@ import com.skeleton.R;
  */
 
 public class LoginFragment extends BaseFragment {
-
+    private EditText etEmail,etPassword;
+    private Button btnSignIn;
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.login_fragment, container, false);
+        init(view);
+
+
         return view;
+    }
+
+    /**
+     *
+     */
+    private void init(View view){
+        etEmail = (EditText) view.findViewById(R.id.etEmail);
+        etPassword = (EditText) view.findViewById(R.id.etPassword);
+        btnSignIn = (Button) view.findViewById(R.id.btnSignin);
     }
 
 
